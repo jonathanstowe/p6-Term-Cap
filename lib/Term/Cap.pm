ee3fd68d00741dc45c7578bb4ec93f4cca86aee3
Jonathan Stowe <jns+git@gellyfish.co.uk>
2015-07-01 18:57:20 +0100

    Add the grammar

--- lib/Term/Cap.pm
+++ lib/Term/Cap.pm
@@ -64,5 +64,122 @@ class Term::Cap {
     multi sub calculate_padding(Int $ospeed) returns Num {
         return (10000 / $ospeed).Num;
     }
+
+    grammar Grammar {
+        rule comment          { ^^\# }
+        rule blank            { ^^\s*$$ }
+        rule comment_or_blank { <comment>|<blank> }
+        rule continuation     { \\\s*$$ }
+        rule empty_cap        { \s*\\\s* }
+        token cap             { \w\w }
+        token num_val         { \d+ }
+        token esc             { \\E }
+        token od              { <[0 .. 7]> }
+        token oct_val         { <od><od><od> }
+        token oct_chr         { \\<oct_val> }
+        token nl              { \\n }
+        token ret             { \\r }
+        token tab             { \\t }
+        token ff              { \\f }
+        token caret           { \\\^ }
+        token ctrl_char       { <:Lu> }
+        token ctrl            { \^<ctrl_char> }
+        token del             { \^\? }
+        token char            { . }
+        token esc_char        { \\<char> }
+        regex special         {  
+                                <value=esc>      || 
+                                <value=oct_chr>  ||
+                                <value=nl>       ||
+                                <value=ret>      ||
+                                <value=tab>      ||
+                                <value=ff>       ||
+                                <value=caret>    ||
+                                <value=ctrl>     ||
+                                <value=del>      ||
+                                <value=esc_char> 
+                              }
+        token literal         { . }
+        token str_val         { [ <value=special> || <value=literal> ]+ }
+        token true_bool       { <name=cap> }
+        token false_bool      { <name=cap>\@ }
+        token num_cap         { <name=cap>\#<value=num_val> }
+        token str_cap         { <name=cap>\=<value=str_val> }
+        token capability      { ^<capability=true_bool>|<capability=false_bool>|<capability=num_cap>|<capability=str_cap>$ }
+        token name            { <-[\|\:]>+ }
+        token names           { <name>+ % '|' }
+        regex record          { ^^ <names> ':' [ <capability> | <continuation> | <empty_cap> ]+ %% ':' }
+        token TOP             { [ <comment_or_blank> || <record> ] + }
+    }
+
+    class Actions  {
+        method cap($/) {
+            $/.make: ~$/;
+        }
+        method num_val($/) {
+            $/.make: ~$/.Int +0;
+        }
+        method esc($/) {
+            $/.make("\o[033]");
+        }
+        method oct_chr($/) {
+            my $val = $/<oct_val>.Str;
+            my $oct_val = :8($val).chr;
+            $/.make($oct_val);
+        }
+        method nl($/) {
+            $/.make("\n");
+        }
+        method ret($/) {
+            $/.make("\r");
+        }
+        method tab($/) {
+            $/.make("\t");
+        }
+        method ff($/) {
+            $/.make("\f");
+        }
+        method caret($/) {
+            $/.make('^');
+        }
+        method del($/) {
+            $/.make("\o[177]");
+        }
+        method ctrl($/) {
+            my $char = $/<ctrl_char>;
+            my $ctrl_char = pack('C',ord($char) +& 31).decode ;
+            $/.make($ctrl_char);
+        }
+        method esc_char($/) {
+            $/.make($/<char>);
+        }
+        method literal($/) {
+            $/.make(~$/);
+        }
+        method special($/) {
+            my $val = $/<value>.made;
+            $/.make($val);
+        }
+        method str_val($/) {
+            my $val = $/<value>.list.map( { .made }).join('');
+            $/.make($val);
+        }
+        method true_bool($/) {
+            $/.make: $<name>.made => True;
+        }
+        method false_bool($/) {
+            $/.make: $<name>.made => False;
+        }
+        method num_cap($/) {
+            $/.make: $<name>.made => $<value>.made;
+        }
+        method str_cap($/) {
+            $/.make: $<name>.made => $<value>.made;
+        }
+        method TOP($/) {
+            $/.make: $<capability>.made;
+        }
+    }
+
 }
 # vim: expandtab shiftwidth=4 ft=perl6
